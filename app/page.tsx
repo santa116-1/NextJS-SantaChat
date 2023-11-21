a94beb6809b9a3577aed9af650ebcddeba2bf071
santa116-1 <totoplife116@gmail.com>
2023-11-22 02:50:24 +0900

    asdasd

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,7 @@ export default function Chat() {
         <div className="absolute w-[360px] left-[50px] top-[100px]">
           {messages.length > 0
             ? messages.map(m => (
-              <div key={m.id} className={`whitespace-pre-wrap ${m.id}`}>
-                {m.role === 'user' ? 'User: ' : 'AI: '}
+              <div key={m.id} className={`whitespace-pre-wrap ${m.role === 'user' ? 'user-message' : 'ai-message'}`}>
                 {m.content}
               </div>
             ))
