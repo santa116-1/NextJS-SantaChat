6a9d0d60baae4a6e0f1ae29074ab6f52710ebaa2
santa116-1 <totoplife116@gmail.com>
2023-11-22 14:13:10 +0900

    change text color white

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Chat() {
       <div className="w-[1200px] h-[800px] bg-gradient-to-bl from-[#edb4a6] to-[#cee6fe] rounded-[50px] opacity-[95%] relative p-[40px]">
         <img src="/images/character.png" alt="" width={400} className="absolute left-[50%] translate-x-[-50%] bottom-0 z-[2]" />
         <div className="absolute w-[360px] h-[600px] overflow-y-auto left-[50px] top-[100px] flex flex-col gap-[10px]">
-          <div className="whitespace-pre-wrap max-w-[280px] p-2 rounded-t-lg ms-auto me-0 bg-[#979ea7] rounded-l-lg">
+          <div className="whitespace-pre-wrap max-w-[280px] p-2 rounded-t-lg ms-auto me-0 bg-[#979ea7] rounded-l-lg text-white">
             Well Hello Ho Ho Ho there,<br />
             how are you?<br />
             What would you like to know?<br />
@@ -17,14 +17,14 @@ export default function Chat() {
           </div>
           {messages.length > 0
             ? messages.map(m => (
-              <div key={m.id} className={`whitespace-pre-wrap max-w-[280px] p-2 rounded-t-lg ${m.role === 'user' ? 'ms-0 me-auto bg-[#94c8f3] rounded-r-lg' : 'ms-auto me-0 bg-[#979ea7] rounded-l-lg'}`}>
+              <div key={m.id} className={`whitespace-pre-wrap max-w-[280px] p-2 rounded-t-lg text-white ${m.role === 'user' ? 'ms-0 me-auto bg-[#94c8f3] rounded-r-lg' : 'ms-auto me-0 bg-[#979ea7] rounded-l-lg'}`}>
                 {m.content}
               </div>
             ))
             : null}
         </div>
-        <div className="">
-          <form onSubmit={handleSubmit}>
+        <div className="w-full">
+          <form className="w-full" onSubmit={handleSubmit}>
             <input
               className="absolute bottom-[20px] z-[3] w-[calc(100%_-_80px)] rounded p-3"
               value={input}
