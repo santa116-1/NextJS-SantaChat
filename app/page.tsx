42d67973479919eb5f8ff1303081c7cdd1f88708
santa116-1 <totoplife116@gmail.com>
2023-11-22 15:08:47 +0900

    increase border-radius

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Chat() {
       <div className="w-[1200px] h-[800px] bg-gradient-to-bl from-[#edb4a6] to-[#cee6fe] rounded-[50px] opacity-[95%] relative p-[40px]">
         <img src="/images/character.png" alt="" width={400} className="absolute left-[50%] translate-x-[-50%] bottom-0 z-[2]" />
         <div className="absolute w-[360px] h-[600px] overflow-y-auto left-[50px] top-[100px] flex flex-col gap-[10px]">
-          <div className="whitespace-pre-wrap max-w-[280px] p-2 rounded-t-lg ms-auto me-0 bg-[#979ea7] rounded-l-lg text-white">
+          <div className="whitespace-pre-wrap max-w-[280px] p-3 rounded-t-[20px] ms-auto me-0 bg-[#979ea7] rounded-l-[20px] text-white">
             Well Hello Ho Ho Ho there,<br />
             how are you?<br />
             What would you like to know?<br />
@@ -18,7 +18,7 @@ export default function Chat() {
           </div>
           {messages.length > 0
             ? messages.map(m => (
-              <div key={m.id} className={`whitespace-pre-wrap max-w-[280px] p-2 rounded-t-lg text-white ${m.role === 'user' ? 'ms-0 me-auto bg-[#94c8f3] rounded-r-lg' : 'ms-auto me-0 bg-[#979ea7] rounded-l-lg'}`}>
+              <div key={m.id} className={`whitespace-pre-wrap max-w-[280px] p-2 rounded-t-[20px] text-white ${m.role === 'user' ? 'ms-0 me-auto bg-[#94c8f3] rounded-r-[20px]' : 'ms-auto me-0 bg-[#979ea7] rounded-l-[20px]'}`}>
                 {m.content}
               </div>
             ))
