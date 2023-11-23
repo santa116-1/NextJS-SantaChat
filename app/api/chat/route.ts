730829d4fb729ae54ebc05adbbef8df6b55e2f46
santa116-1 <totoplife116@gmail.com>
2023-11-23 16:37:53 +0900

    changed the code

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -11,12 +11,17 @@ const openai = new OpenAI({
 export const runtime = 'edge';
 
 export async function POST(req: Request) {
-  // Extract the `prompt` from the body of the request
-  const { messages } = await req.json();
+  // Extract the `userMessages` from the body of the request
+  const { userMessages } = await req.json();
 
-  console.log(messages);
+  const messages = [
+    { role: 'system', content: '日本語で返信する' },
+    ...userMessages.map((message: string) => ({ role: 'user', content: message })),
+  ];
 
-  // Ask OpenAI for a streaming chat completion given the prompt
+  console.log(userMessages);
+
+  // Ask OpenAI for a streaming chat completion given the messages
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
     stream: true,
