715408aa7727fc85cf0cfbebff94e43cc5e72a7b
santa116-1 <totoplife116@gmail.com>
2023-11-29 17:05:19 +0900

    remove overflow hide

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} overflow-hidden`}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
