40a5c4a8cb0ba854ba05a59a5ec1f90d68c5c356
santa116-1 <totoplife116@gmail.com>
2023-12-14 18:01:53 +0900

    increased bottom

--- app/page.tsx
+++ app/page.tsx
@@ -106,13 +106,13 @@ export default function Chat() {
             <div className="w-full">
               <form className="w-full" onSubmit={handleSubmit}>
                 <input
-                  className="absolute bottom-[20px] max-[450px]:bottom-[70px] z-[10] w-[calc(100%_-_80px)] rounded-[25px] p-3 outline-none bg-[#fff] max-[1200px]:p-2 max-[1200px]:text-[14px] max-[1200px]:left-[40px] max-[450px]:pr-[45px] start-img"
+                  className="absolute bottom-[20px] max-[450px]:bottom-[90px] z-[10] w-[calc(100%_-_80px)] rounded-[25px] p-3 outline-none bg-[#fff] max-[1200px]:p-2 max-[1200px]:text-[14px] max-[1200px]:left-[40px] max-[450px]:pr-[45px] start-img"
                   value={input}
                   placeholder="What will I get for Christmas?"
                   onChange={handleInputChange}
                   autoComplete="no-safari-autofill"
                 />
-                <Image src="/images/right-arrow.png" className='absolute bottom-[30px] right-[50px] cursor-pointer z-[11] max-[1200px]:bottom-[72px]' width={30} height={30} quality={100} alt="submit" onClick={(e) => handleSubmit(e as any)} />
+                <Image src="/images/right-arrow.png" className='absolute bottom-[30px] right-[50px] cursor-pointer z-[11] max-[1200px]:bottom-[92px]' width={30} height={30} quality={100} alt="submit" onClick={(e) => handleSubmit(e as any)} />
               </form>
             </div>
           </>
