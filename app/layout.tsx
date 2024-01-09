94ad50f882033f484bce5b0d2586b5aa320c44eb
santa116-1 <totoplife116@gmail.com>
2024-01-09 14:44:19 +0900

    changed title and desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-
 import './globals.css';
-import './custom.css';
 import { Inter } from 'next/font/google';
-import Header from './header';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: '1 if by land christmas 2023_main',
+  description: '1 if by land christmas 2023_main',
+};
 
 export default function RootLayout({
   children,
@@ -14,8 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Header />
-      <body className={`${inter.className} overflow-hidden fixed w-full`}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
