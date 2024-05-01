314a429201b82b95828a7e22aa137e06520ac36b
santa116-1 <totoplife116@gmail.com>
2024-05-01 22:29:49 +0900

    update the heading

--- app/header.tsx
+++ app/header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
 	return (
 		<header>
-			<title>SantaChattingGift</title>
+			<title>SantaChatting</title>
 			<meta name="description" content="Santa Chatting Gift" />
 			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
 		</header>
